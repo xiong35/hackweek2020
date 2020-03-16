205e7e79a58b0af7982f6a0f24e254b31ca60895
xiong35 <2242787668@qq.com>
2020-03-16 22:25:24 +0800

    get population

--- population.py
+++ population.py
@@ -1,10 +1,24 @@
 
 import pandas as pd
+import json
 
 filename = R'.\population.csv'
 
-df = pd.read_csv(filename,header=0)
-df = pd.DataFrame(list(df['population']),index=df['Country Name'])
-print(int(df.loc['China']))
+population = pd.read_csv(filename,header=0)
+population = pd.DataFrame(list(population['population']),index=population['Country Name'])
 
 
+with open(R'.\world_data.json') as f:
+    lines = f.readlines()
+
+json_str = ''
+for line in lines:
+    json_str += line
+
+world_data_dict = json.loads(json_str)
+
+for country, info_dic in world_data_dict.items():
+    try:
+        a= df.loc[country]
+    except KeyError:
+        print('['+country+']',' not found')
